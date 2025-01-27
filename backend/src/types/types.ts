1d4dd361a1b06efdf6c5e62189c50bf0d7cab410
Aman Thukral <thukral618@gmail.com>
2025-01-28 03:50:36 +0530

    Add passport config for google auth along with db config to initialize db

---
+++ backend/src/types/types.ts
@@ -0,0 +1,54 @@
+export interface CompanyDocument {
+  id: number;
+  googleId: string;
+  email: string;
+  avatar: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface SessionDocument {
+  id: string;
+  sessionId: string;
+  companyId: string;
+  refreshToken: string;
+  deviceInfo: string;
+  createdAt: Date;
+  updatedAt: Date;
+  expiresAt: Date;
+  lastUsedAt: Date;
+  company: CompanyDocument;
+}
+
+export interface CustomSession {
+  id: string;
+  sessionId: string;
+  companyId: number;
+  deviceInfo: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+  expiresAt: Date;
+  lastUsedAt: Date;
+}
+
+export interface GoogleStrategyOptions {
+  clientID: string;
+  clientSecret: string;
+  callbackURL: string;
+}
+
+export interface GoogleStrategyOptionsWithRequest
+  extends GoogleStrategyOptions {
+  passReqToCallback: true;
+}
+
+export interface VerifyCallbackDocument {
+  company?: CompanyDocument;
+  sessionId: string;
+}
+
+export type VerifyCallback = (
+  error: any,
+  company?: VerifyCallbackDocument,
+  info?: any
+) => void;
