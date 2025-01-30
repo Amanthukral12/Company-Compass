236b47acc39abde6da741c3a338c4f54834791dd
Aman Thukral <thukral618@gmail.com>
2025-01-31 02:08:57 +0530

    Add rest of the employee controller functions

--- backend/src/controller/employee.controller.ts
+++ backend/src/controller/employee.controller.ts
@@ -38,3 +38,117 @@ export const createEmployee = asyncHandler(
     }
   }
 );
+
+export const getAllEmployees = asyncHandler(
+  async (req: Request, res: Response) => {
+    if (!req.company) {
+      throw new ApiError(401, "Unauthorized Access. Please login again", [
+        "Unauthorized Access. Please login again",
+      ]);
+    }
+    const companyId = req.company.id;
+    const employees = await prisma.employee.findMany({
+      where: {
+        companyId,
+      },
+    });
+    return res
+      .status(200)
+      .json(new ApiResponse(200, employees, "Employees fetched"));
+  }
+);
+
+export const getEmployeeById = asyncHandler(
+  async (req: Request, res: Response) => {
+    if (!req.company) {
+      throw new ApiError(401, "Unauthorized Access. Please login again", [
+        "Unauthorized Access. Please login again",
+      ]);
+    }
+    const companyId = req.company.id;
+    const { employeeId } = req.params;
+
+    const employee = await prisma.employee.findUnique({
+      where: { id: Number(employeeId), companyId },
+    });
+
+    if (!employee) {
+      throw new ApiError(404, "Employee not found", ["Employee not found"]);
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, employee, "Employee fetched successfully"));
+  }
+);
+
+export const deleteEmployee = asyncHandler(
+  async (req: Request, res: Response) => {
+    if (!req.company) {
+      throw new ApiError(401, "Unauthorized Access. Please login again", [
+        "Unauthorized Access. Please login again",
+      ]);
+    }
+    const companyId = req.company.id;
+    const { employeeId } = req.params;
+
+    const employee = prisma.employee.findUnique({
+      where: {
+        id: Number(employeeId),
+        companyId,
+      },
+    });
+
+    if (!employee) {
+      throw new ApiError(404, "Employee not found", ["Employee not found"]);
+    }
+
+    await prisma.employee.delete({
+      where: {
+        id: Number(employeeId),
+        companyId,
+      },
+    });
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Employee deleted successfully"));
+  }
+);
+
+export const updateEmployee = asyncHandler(
+  async (req: Request, res: Response) => {
+    if (!req.company) {
+      throw new ApiError(401, "Unauthorized Access. Please login again", [
+        "Unauthorized Access. Please login again",
+      ]);
+    }
+    const companyId = req.company.id;
+    const { employeeId } = req.params;
+    const { name, phoneNumber } = req.body;
+
+    const employeeExists = prisma.employee.findUnique({
+      where: {
+        id: Number(employeeId),
+        companyId,
+      },
+    });
+
+    if (!employeeExists) {
+      throw new ApiError(404, "Employee not found", ["Employee not found"]);
+    }
+
+    const employee = await prisma.employee.update({
+      where: {
+        companyId,
+        id: Number(employeeId),
+      },
+      data: {
+        name: name !== undefined ? name : undefined,
+        phoneNumber: phoneNumber !== undefined ? phoneNumber : undefined,
+      },
+    });
+    return res
+      .status(200)
+      .json(new ApiResponse(200, employee, "Employee updated successfully"));
+  }
+);
--- backend/src/routes/employee.routes.ts
+++ backend/src/routes/employee.routes.ts
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import { authenticateSession } from "../middleware/auth";
-import { createEmployee } from "../controller/employee.controller";
+import {
+  createEmployee,
+  deleteEmployee,
+  getAllEmployees,
+  getEmployeeById,
+  updateEmployee,
+} from "../controller/employee.controller";
 
 const router = Router();
 
 router.route("/add").post(authenticateSession, createEmployee);
+router.route("/").get(authenticateSession, getAllEmployees);
+router
+  .route("/:employeeId")
+  .get(authenticateSession, getEmployeeById)
+  .delete(authenticateSession, deleteEmployee)
+  .put(authenticateSession, updateEmployee);
 
 export default router;
